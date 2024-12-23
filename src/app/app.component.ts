36f94cd2257919e4a625eec72235f0b7151fd37d
matheus mozart <mmsnborges@gmail.com>
2024-12-22 21:38:10 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, FooterComponent]
+})
+export class AppComponent {
+  title = 'monitor-front';
+}
